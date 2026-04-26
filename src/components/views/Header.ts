3ed2325fec9d942b8eb5131eaac8cdf0fac256d1
RAINEY5900 <topbs809@gmail.com>
2026-04-26 19:36:57 +0500

    fix: 2

--- src/components/views/Header.ts
+++ src/components/views/Header.ts
@@ -2,21 +2,23 @@ import { Component } from "../base/Component";
 import { IEvents } from "../base/Events";
 
 export class Header extends Component<any> {
-    private _cartButton: HTMLElement;
-    private _counter: HTMLElement;
+    private _cartButton: HTMLElement | null;
+    private _counter: HTMLElement | null;
 
     constructor(container: HTMLElement, private events: IEvents) {
         super(container);
-        this._cartButton = container.querySelector('.header__cart') as HTMLElement;
-        this._counter = container.querySelector('.header__cart-counter') as HTMLElement;
-
-        this._cartButton.addEventListener('click', () => {
-            this.events.emit('cart:opened');
-        });
+        this._cartButton = container.querySelector('.header__basket');
+        this._counter = container.querySelector('.header__basket-counter');
+        
+        if (this._cartButton) {
+            this._cartButton.addEventListener('click', () => {
+                this.events.emit('cart:opened');
+            });
+        }
     }
 
-    render(data: any): HTMLElement {
-        if (data.count !== undefined && this._counter) {
+    render(data: { count: number }): HTMLElement {
+        if (this._counter && data.count !== undefined) {
             this._counter.textContent = String(data.count);
         }
         return this._container;
